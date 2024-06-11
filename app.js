95711dbb7db89d5e53223b20dfd368380cf8e1c7
ChowdamAshok <161301092+ChowdamAshok@users.noreply.github.com>
2024-06-11 21:39:21 +0530

    app.js

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ function calculateResult() {
     }
 }
 
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        document.body.classList.remove('loading');
-        document.querySelector('.loading-overlay').style.display = 'none';
-    }, 2000);
-});
+
 
 gsap.from('.heading',{opacity: 0, duration: 1, delay:1.5, y:30, stagger:0.2})
